71b55dbb3538a6e795425abe926806d0364836fe
thease94 <sebastx17@gmail.com>
2017-11-21 10:53:20 +0100

    test2

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hehe formularz</title>
+  </head>
+  <body>
+    <form action="form_action.php" method="post">
+      <input type="text" name="name" value="" placeholder="Name...">
+      <input type="password" name="passwd" value="" placeholder="Password...">
+      <input type="submit" name="" value="">
+    </form>
+  </body>
+</html>
